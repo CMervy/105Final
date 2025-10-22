5d7dff3397628ee874d1e54f76306f12ed5979cb
CMervy <connerm05@gmail.com>
2025-10-22 10:54:58 -0400

    html and css done

---
+++ script.js
@@ -0,0 +1,9 @@
+const list = [];
+const listItems = document.getElementById(`List`)
+const newItem = document.getElementById(`newitem`)
+const addBtn = document.getElementById(`addbtn`)
+const allBtn = document.getElementById(`ShowAll`)
+const unpurchasedBtn = document.getElementById(`unpurchased`)
+const purchasedBtn = document.getElementById(`purchased`)
+
+function
