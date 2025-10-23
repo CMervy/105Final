2c8888e0ec90b601e743b7b26a87e38f13b99bfc
CMervy <connerm05@gmail.com>
2025-10-23 08:34:05 -0400

    done

--- script.js
+++ script.js
@@ -6,4 +6,41 @@ const allBtn = document.getElementById(`ShowAll`)
 const unpurchasedBtn = document.getElementById(`unpurchased`)
 const purchasedBtn = document.getElementById(`purchased`)
 
-function
+function refreshList(filter = `all`){
+    listItems.innerHTML = ``;
+
+    list.forEach((item, index) => {
+        if(filter === `unpurchased` && item.purchased) return;
+        if(filter === `purchased` && !item.purchased) return;
+
+        const li = document.createElement(`li`);
+        li.textContent = item.name;
+        if(item.purchased)
+            li.classList.add(`purchased`);
+
+        li.addEventListener(`click`, () =>{
+            item.purchased = !item.purchased;
+            refreshList(filter)
+        })
+
+        listItems.appendChild(li);
+    })
+}
+addBtn.addEventListener(`click`, () => {
+    const itemName = newItem.value.trim();
+    if(itemName){
+        list.push({name: itemName, purchased: false});
+        newItem.value = ``
+        refreshList();
+    }
+})
+allBtn.addEventListener(`click`, () =>{
+    refreshList(`all`)
+})
+unpurchasedBtn.addEventListener(`click`, () =>{
+    refreshList(`unpurchased`)
+})
+purchasedBtn.addEventListener(`click`, () =>{
+    refreshList(`purchased`)
+})
+
